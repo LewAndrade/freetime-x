2b11cca3f1b988ea9bbabfd7930845b89b315e93
Lew <andrade.lew@gmail.com>
2024-02-15 23:07:21 -0300

    feat: fly deployment settings

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,13 @@ import { Home } from "./pages/home";
 import { Client } from "@notionhq/client";
 import { staticPlugin } from "./plugins/static";
 import { activityPlugin } from "./plugins/activity";
-
+const port = process.env.PORT || "8080"
 const app = new Elysia()
   .use(html())
-  // .get("/", () => <Home />)
   .get("/*", () => <Home/>)
   .use(activityPlugin)
   .use(staticPlugin)
-  .listen(3000);
+  .listen(port);
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
